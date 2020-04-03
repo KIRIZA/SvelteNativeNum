71c3eb5294a986276b12598b4ec475396b77a33a
KIRIZA <48911288+KIRIZA@users.noreply.github.com>
2020-04-03 22:52:05 +0200

    Add files via upload

---
+++ app/App.svelte
@@ -0,0 +1,33 @@
+<page actionBarHidden={true}>
+   <tabs bind:selectedIndex={selectedTab} tabsPosition='bottom'>
+	<tabStrip class='blue'>
+		<tabStripItem>
+			<label text="Page 1" />
+		</tabStripItem>
+		</tabStrip>
+
+	<tabContentItem>
+        <cardView margin='32' elevation='40'>
+            <Page1 />
+        </cardView>
+	</tabContentItem>       
+</tabs>
+</page>
+
+<script>
+//first: tns plugin add @nstudio/nativescript-cardview
+import { registerNativeViewElement } from 'svelte-native/dom' 
+registerNativeViewElement("cardView", () => 
+    require("@nstudio/nativescript-cardview").CardView
+)
+    import Page1 from './screens/Page1.svelte'
+    
+    let selectedTab = 0
+</script>
+
+<style>
+    .blue{
+        padding:24;
+        }
+    
+</style>
---
+++ app/screens/Page3.svelte
@@ -0,0 +1,24 @@
+<flexboxLayout class='flex-centered'>
+    <label class='h1 text-center' text='page 3' />
+        <cardView margin='32' elevation='40'>
+            <button text='page 2' on:tap = { () => selectedTab = 0 } />
+        </cardView>
+</flexboxLayout>
+
+<script>
+//    import Chart from './Chart.svelte';
+    export let selectedTab
+    let data = [1,2,3,4,5,6]
+</script>
+
+<stackLayout>
+    <textField type="number" bind:text={data[0]} />
+    <textField type="number" bind:text={data[1]} />
+    <textField type="number" bind:text={data[2]} />
+    <textField type="number" bind:text={data[3]} />
+    <textField type="number" bind:text={data[4]} />
+    <textField type="number" bind:text={data[5]} />
+</stackLayout>
+
+<!-- <Chart {data} /> -->
+
